00999fe0e1cfe640bd028befaba25c4de75081ef
ll9 <llwierer@web.de>
2019-02-13 19:04:24 +0100

    init

---
+++ EFCore_DataGrid_Sync/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using EFCore_DataGrid_Sync.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_DataGrid_Sync.Data
+{
+    class ApplicationDbContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=db.sqlite");
+        }
+    }
+}
