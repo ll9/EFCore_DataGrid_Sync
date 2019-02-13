f6238068d6e09cf1490fffc93415714669793220
ll9 <llwierer@web.de>
2019-02-13 19:13:53 +0100

    Basic EF Datagridview example

--- EFCore_DataGrid_Sync/Form1.cs
+++ EFCore_DataGrid_Sync/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EFCore_DataGrid_Sync.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,21 @@ namespace EFCore_DataGrid_Sync
 {
     public partial class Form1 : Form
     {
+        private ApplicationDbContext _context;
+
         public Form1()
         {
             InitializeComponent();
+            _context = new ApplicationDbContext();
+
+            dataGridView1.DataSource = _context.People.Local.ToBindingList();
+            _context.People.Load();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.EndEdit();
+            _context.SaveChanges();
         }
     }
 }
